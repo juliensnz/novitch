870b8df6fff75bc944ff79636ae5a41fbbd0513f
Julien Sanchez <sumbobyboys@gmail.com>
2020-01-05 22:15:04 +0100

    Updates

---
+++ precache-manifest.63bdd6615bd19743b5985150d3c5af87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3f7b97a83556187c6f6545f439f3f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59180918245bad3f17d6",
+    "url": "/static/js/2.3c585460.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.3c585460.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6278046d2d4525e8182e",
+    "url": "/static/js/main.b7677895.chunk.js"
+  },
+  {
+    "revision": "9d614be1e31eea4a02e3",
+    "url": "/static/js/runtime-main.a872d95c.js"
+  }
+]);
